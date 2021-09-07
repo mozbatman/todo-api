aa2456d24c3fae3555e0020eaaba388373caa9b4
mozbatman <mustafa@digiterra.com.tr>
2021-09-07 15:05:26 +0300

    Update port.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := router.Router()
 	// fs := http.FileServer(http.Dir("build"))
 	// http.Handle("/", fs)
-	fmt.Println("Starting server on the port 8080...")
+	fmt.Println("Starting server on the port 5000...")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":5000", r))
 }
